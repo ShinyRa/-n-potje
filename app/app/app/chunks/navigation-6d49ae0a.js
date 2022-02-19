d4328d6f16fe8e18084d0489002d75b29ec244f3
tijs <undefined>
2022-02-19 21:27:40 +0100

    Updates

--- app/app/app/chunks/navigation-6d49ae0a.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./singletons-82f8d8c8.js";const g=n;async function n(o,t){return r.goto(o,t,[])}export{g};
--- app/app/chunks/singletons-82f8d8c8.js
+++
@@ -1,1 +0,0 @@
-let t="",e="";function r(s){t=s.base,e=s.assets||t}let a;function i(s){a=s.router}export{e as a,t as b,i,a as r,r as s};
